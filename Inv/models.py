695d731ff66a97acfd38bba7e202c956a2bca8b1
cihuaxty <cihuaxtysamperio@gmail.com>
2020-06-08 16:57:14 -0500

    Seccion categoria concluida

--- Inv/models.py
+++ Inv/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from bases.models import ClaseModelo
+
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripcion de la categoria',
+        unique=True
+    )
+
+    def __str__(self):
+        #cuando se cargue la categoria hag referencia a su descripcion
+        return '{}'.format(self.descripcion)
+
+    def save(self):
+        #guardar en mayusculas la descripcion
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).save() #invoca al metodo para poder guardar el dato en mayusculas
+
+    class Meta:
+        verbose_name_plural = "Categorias"
--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    fc = models.DateTimeField(auto_now_add=True) #fecha de creacion solo se modifica una vez
+    fm = models.DateTimeField(auto_now=True) #fecha que se modifica en cada transancion
+    uc = models.ForeignKey(User, on_delete=models.CASCADE) #usuario que crea el registro
+    um = models.IntegerField(blank=True, null= True) #usuario que modifica el registro, puede estar en blanco y ser nulo
+
+    #Clase para que este modelo sea del tipo abstacto y no sea tomado en cuenta en la migracion
+    class Meta:
+        abstract=True
